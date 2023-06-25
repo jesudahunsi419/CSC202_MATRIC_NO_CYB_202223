97b262c1335695533bd78bbdc627965777bc0769
Victor Dahunsi <victorjesudahunsi@gmail.com>
2023-06-25 22:29:38 +0100

    Python

---
+++ GENIN/WEEK ONE/.Awoseyi.py
@@ -0,0 +1,19 @@
+print("Hello world!")
+print("Day 1 - Python Print Function")
+print("print('what to print')")
+print("New lines can be created with a backslash and n.")
+input("What is your name?")
+print("Hello " + input("what is your name?"))
+print(len("Victor"))
+name = input("What is your name?")
+print(name)
+a = input("a:")
+b = input("b:")
+print("a = " + a)
+print("b = " + b)
+c = a
+a = b
+b = c
+
+print("Welcome to the band name generator.")
+city = input("Which city did you grow up in?")
